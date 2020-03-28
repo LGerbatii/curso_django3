c5d5e4a5d1b1c646ac676b6311c39c67968b759b
unknown <lgerbati@gmail.com>
2020-03-28 20:16:06 -0300

    Aula 11

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello_blog(request):
+    lista = [
+        'Django', 
+        'Python', 
+        'git',
+    ]
+    data = {'name': 'Curso de Django 3', 'lista_tecnologia':lista}
+    return render(request, 'index.html', data)
