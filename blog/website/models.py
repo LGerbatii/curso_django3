cb9f257d5bd12923c49492105aee4415c4001864
unknown <lgerbati@gmail.com>
2020-03-29 13:26:54 -0300

    template

--- blog/website/models.py
+++ blog/website/models.py
@@ -1,6 +1,31 @@
 from django.db import models
 
+
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidade'
+    GR = 'GR', 'Geral'
+
+    
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR
+    )
+
+    deleted = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+    def full_name(self):
+        return self.title + self.subtitle
+
+    full_name.admin_order_field = 'title'
--- blog/website/views.py
+++ blog/website/views.py
@@ -9,10 +9,14 @@ def hello_blog(request):
         'git',
     ]
 
-    list_posts = Post.objects.all()
+    list_posts = Post.objects.filter(deleted=False)
 
     data = {
         'name': 'Curso de Django 3',
         'lista_tecnologia':lista,
         'post':list_posts,}
     return render(request, 'index.html', data)
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'post_detail.html', {'post':post})
